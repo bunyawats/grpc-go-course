e24d30fcadb079f1040a5da1d1ea866237648b77
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-01-07 21:18:53 +0700

    17. Server Setup Boilerplate Code

---
+++ generator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+protoc greet/greetpb/greet.proto --go-grpc_out=./greet --go_out=./greet
+
+protoc calculator/calculatorpb/calculator.proto --go-grpc_out=./calculator --go_out=./calculator
+protoc blog/blogpb/blog.proto --go-grpc_out=./blog --go_out=./blog
