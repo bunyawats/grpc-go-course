c7492708f981d0974d0d386ec168c0c7011f09dc
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-01-13 20:01:26 +0700

    separate the RPC implementation out of the server boilerplate.

---
+++ greet/greet_server/rpc_impl.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpc-go-course/greet/greetpb"
+	"io"
+	"log"
+	"strconv"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type server struct {
+	greetpb.UnimplementedGreetServiceServer
+}
+
+func (*server) Greet(ctx context.Context,
+	req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+
+	// fmt.Printf("Greet function was invoked with %v\n", req)
+	firstName := req.Greeting.GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Resulte: result,
+	}
+	return res, nil
+}
+
+func (*server) GreetManyTime(req *greetpb.GreetManyTimeRequest,
+	stream greetpb.GreetService_GreetManyTimeServer) error {
+
+	// fmt.Printf("GreetManyTime RPC was invocked with %v\n", req)
+	firstName := req.Greeting.FirstName
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimeResponse{
+			Resulte: result,
+		}
+		stream.Send(res)
+		time.Sleep(time.Second)
+	}
+	return nil
+}
+
+func (*server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
+
+	// fmt.Printf("LongGreet RPC was invocked with stream requested")
+	result := ""
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// we has finish reading the client
+			res := &greetpb.LongGreetResponse{
+				Resulte: result,
+			}
+			return stream.SendAndClose(res)
+
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client strean: %v\n", err)
+		}
+		result += "Hello " + req.GetGreeting().GetFirstName() + "!"
+	}
+}
+
+func (*server) GreetEveryOne(stream greetpb.GreetService_GreetEveryOneServer) error {
+
+	// fmt.Printf("GreetEveryOne RPC was invocked with stream requested")
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			// we has finish reading the client
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client strean: %v\n", err)
+			return err
+		}
+		firstName := req.GetGreeting().GetFirstName()
+		result := "Hello " + firstName + "!"
+		sendErr := stream.Send(&greetpb.GreetEveryOneResponse{
+			Resulte: result,
+		})
+		if sendErr != nil {
+			log.Fatalf("Error while sending data to client: %v\n", err)
+			return sendErr
+		}
+	}
+}
+
+func (*server) GreetWithDeadline(
+	ctx context.Context, req *greetpb.GreetWithDeadlineRequest) (
+	*greetpb.GreetWithDeadlineResponse, error) {
+
+	// fmt.Printf("Greet with Deadline function was invoked with %v\n", req)
+	for i := 0; i < 3; i++ {
+		if ctx.Err() == context.Canceled {
+			fmt.Println("The client cancel the request!")
+			return nil, status.Error(
+				codes.Canceled,
+				"The client cancel the request!")
+		}
+		time.Sleep(time.Second)
+	}
+
+	firstName := req.Greeting.GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetWithDeadlineResponse{
+		Resulte: result,
+	}
+	return res, nil
+}
--- server.sh
+++ server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-go run greet/greet_server/server.go
-
+go run greet/greet_server/server.go greet/greet_server/rpc_impl.go
 # go run calculator/calculator_server/server.go
 
 # go run blog/blog_server/server.go
