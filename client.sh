0e4b5ceefbee082c396ada94a51aaef6fff2325c
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-01-13 12:51:54 +0700

    add start up script

---
+++ client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go run greet/greet_client/client.go
+
+# go run calculator/calculator_client/client.go
+
+# go run blog/blog_client/client.go
---
+++ server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+go run greet/greet_server/server.go
+
+# go run calculator/calculator_server/server.go
+
+# go run blog/blog_server/server.go
+
